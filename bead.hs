f3ad9eda024743f194814e321478607e955de3a0
Domi <vataidomonkos@gmail.com>
2023-01-18 22:13:05 +0100

    still failing hard

--- bead.hs
+++ bead.hs
@@ -2,9 +2,29 @@
 
 import Data.Char
 import Data.List
+import Text.Read
 
 isDecimalDigit :: Char -> Bool
 isDecimalDigit = isDigit
 
 isInt :: String -> Bool
-isInt (x:xs) = isDigit x && isInt xs 
+isInt str 
+    | str == "" = False
+    | str == (x:"") = isDigit x
+    | str == (x:xs) = isDigit x && isInt xs
+
+
+--isInt "" = False
+--isInt (x:"") = isDigit x
+--isInt (x:xs)  = isDigit x && isInt xs
+
+
+
+
+
+
+
+-- case dropWhile isDigit xs of
+--                        ""       -> True
+--                        ('.':ys) -> all isDigit ys
+--                        _        -> False
